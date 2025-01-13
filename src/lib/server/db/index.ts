53e0e492c806d6794a0cba7c0ed2263940815ea7
Neville Brem <bremneville@gmail.com>
2025-01-13 20:51:00 +0100

    schema

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,6 +1,7 @@
 import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 import { DATABASE_URL } from '$env/static/private';
+import * as schema from './schema';
 
 const client = neon(DATABASE_URL);
-export const db = drizzle(client);
+export const db = drizzle(client, { casing: 'snake_case', schema });
--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,51 @@
-import { pgTable, serial, text, integer } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
+import { pgTable, text, integer, timestamp, boolean } from 'drizzle-orm/pg-core';
+
+const UUID = text()
+	.$defaultFn(() => crypto.randomUUID())
+	.primaryKey();
+
+export const wettbewerb = pgTable('wettbewerb', {
+	id: UUID,
+	name: text().notNull(),
+	startsAt: timestamp().notNull(),
+	endsAt: timestamp().notNull(),
+	creatorId: text().references(() => user.id)
+});
+
+export const disziplin = pgTable('disziplin', {
+	id: UUID,
+	name: text().notNull(),
+	faktor: integer().notNull(),
+	wettbewerbId: text().references(() => wettbewerb.id)
+});
 
 export const user = pgTable('user', {
-	id: serial('id').primaryKey(),
-	age: integer('age')
+	id: UUID,
+	username: text().notNull().unique(),
+	admin: boolean().default(false)
 });
+
+export const entry = pgTable('entry', {
+	id: UUID,
+	disziplinId: text()
+		.references(() => disziplin.id)
+		.notNull(),
+	wettbewerbId: text()
+		.references(() => wettbewerb.id)
+		.notNull(),
+	userId: text()
+		.references(() => user.id)
+		.notNull()
+});
+
+export const wettbewerbRel = relations(wettbewerb, ({ many, one }) => ({
+	teilnehmer: many(user),
+	diszipline: many(disziplin),
+	entries: many(entry)
+}));
+
+export const userRel = relations(user, ({ many }) => ({
+	entries: many(entry),
+	wettbewerbe: many(wettbewerb)
+}));

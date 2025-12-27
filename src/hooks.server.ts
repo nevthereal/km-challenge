5ad9da0029c0df929232699c9e3bdb0d80bb184e
Neville Brem <contact@nevillebrem.com>
2025-12-27 18:59:11 +0100

    added redirect for non-www routes
    Signed-off-by: Neville Brem <contact@nevillebrem.com>

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,15 @@
-import { building } from '$app/environment';
+import { building, dev } from '$app/environment';
 import { auth } from '$lib/auth';
+import { redirect } from '@sveltejs/kit';
 import { svelteKitHandler } from 'better-auth/svelte-kit';
 
 export async function handle({ event, resolve }) {
+	const host = event.request.headers.get('host');
+
+	if (host === 'km-challenge.com' && !dev) {
+		throw redirect(301, `https://www.km-challenge.com${event.url.pathname}${event.url.search}`);
+	}
+
 	const session = await auth.api.getSession({
 		headers: event.request.headers
 	});
